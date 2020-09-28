f5e2e16eb790944c259912388ef15e3aa3124701
family <214151436@qq.com>
2020-09-28 15:10:44 +0800

    day3

--- src/app10/app/app.js
+++ src/app10/app/app.js
@@ -1,1 +1,26 @@
+const Koa = require('koa');
+const path = require('path');
+const combineRouters = require('koa-combine-routers');
+const bodyParser = require('koa-bodyparser');
+const koaStatic = require('koa-static');
+const koaCompress = require('koa-compress');
 
+const app = new Koa();
+
+const userRouter = require('../router/userRouter');
+
+app.use(koaCompress({
+    threshold: 2048
+}))
+
+app.use(bodyParser());
+
+app.use(koaStatic(path.join(__dirname,'../dist')));
+
+const unifiedRouters = combineRouters(
+   userRouter
+)();
+
+app.use(unifiedRouters);
+
+module.exports = app;
---
+++ src/app10/scripts/server.js
@@ -0,0 +1,3 @@
+require('../app/app').listen(3000);
+
+console.log('koa2 server started on port 3000');
